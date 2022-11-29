29c93455d5a6ddeb9b9e70910444ce4652a4f58a
Victor Marques <100208584+projeto-victor@users.noreply.github.com>
2022-11-29 08:54:19 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -1,3 +1,35 @@
-funtion StorageManager(a,b){
+function soma(a,b){
     return a + b
+}
+
+function sub(a,b){
+    return a - b
+}
+
+function div(a,b){
+    return a * b
+}
+
+function mult(a,b){
+    return a / b
+}
+
+function decisao(resp, pri, seg){
+    if(resp == 1){
+        resultado = soma(pri, seg)
+        document.write("<p>A soma é " + resultado)
+    }
+    if(resp == 2){
+        resultado = sub(pri, seg)
+        document.write("<p>A subtração é " + resultado)
+    }
+    if(resp == 3){
+        resultado = mult(pri, seg)
+        document.write("<p>A multiplicação é " + resultado)
+    }
+    if(resp == 4){
+        resultado = div(pri, seg)
+        document.write("<p>A divisão é " + resultado)
+    }
+
 }
